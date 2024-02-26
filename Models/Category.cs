cab1a96bdf1dc242c6df25d7afd247125e9ac11f
Vinselcia <153624107+Vinselcia@users.noreply.github.com>
2024-02-26 14:47:28 +0530

    Models

---
+++ Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace PizzaShop.Models
+{
+    public class Category
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Slug { get; set; }
+    }
+}
